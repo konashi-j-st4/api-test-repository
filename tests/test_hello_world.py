acd76a468a1c8c683f4b87404aa21e653d19e4a8
konashi-j-st4 <konashi@st4system.com>
2025-01-02 17:10:03 +0900

    commit 11

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,14 +1,18 @@
 import os
 import sys
+from flask import Flask
 
 sys.path.append(os.environ["REPOSITORY_HOME"] + "/route/dashb/user")
 import dashboard_user_router
 
+# テスト用のFlaskアプリケーション作成
+app = Flask(__name__)
+app.register_blueprint(dashboard_user_router)
 
-def test_ok ():
-
-
-    res = dashboard_user_router.user_home()
-
-    status_code = res['statusCode']
-    assert status_code == 200
+def test_ok():
+    with app.app_context():  # アプリケーションコンテキストを作成
+        res = dashboard_user_router.user_home()
+        response_data = res.get_json()  # JSONレスポンスを取得
+        
+        status_code = response_data['statusCode']
+        assert status_code == 200
